b6e164900c109512ca280d568d2e97a00a46cdc2
pooranjoyb <pooranjoyb2016@gmail.com>
2024-02-18 16:28:00 +0530

    Setup routes and reduced img size

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,23 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
 // Screens
 import Home from "./screens/Home"
+import Shop from "./screens/Shop";
+import ProductDetail from "./screens/ProductDetail";
+import Cart from "./screens/Cart";
 
 function App() {
 
   return (
     <>
-      <>
-        <Home />
-      </>
+      <Router>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/shop" element={<Shop />} />
+            <Route path="/product" element={<ProductDetail />} />
+            <Route path="/cart" element={<Cart />} />
+          </Routes>
+        </Router>
     </>
   )
 }
