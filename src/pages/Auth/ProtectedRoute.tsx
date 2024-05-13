3452d4ced9961d7b09ce64ae02d1bd43dd9848b8
Shashank <shashanksinghnegi12@gmail.com>
2024-05-13 20:46:16 +0530

    resolved dependency issue

--- src/pages/Auth/ProtectedRoute.tsx
+++ src/pages/Auth/ProtectedRoute.tsx
@@ -1,23 +1,14 @@
 import { useSelector } from "react-redux";
-import { useNavigate } from "react-router-dom";
 import { RootState } from "../../utils/features/store";
-
+import Error from "../Error";
 interface ProtectedRouteProps {
   children: React.ReactNode;
 }
 
 export default function ProtectedRoute({ children }: ProtectedRouteProps) {
   const isAuthenticated = useSelector((state: RootState) => state.auth.isAuthenticated);
-  const navigate = useNavigate();
-
-  function handleAuth()
-  {
-    navigate('/');
-  }
-
- 
-  
-  return isAuthenticated !== undefined ? (isAuthenticated ? children : <h1>You Don't Have Valid Access to go on login page Click on <button onClick={handleAuth} className="bg-red-400">Login page</button> .</h1>) : null; // Conditional rendering based on state availability
+   
+  return isAuthenticated !== undefined ? (isAuthenticated ? children : <Error/>) : null; // Conditional rendering based on state availability
   
 
 }
