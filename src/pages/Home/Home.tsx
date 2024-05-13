3ca689198e5a94eec319335e716a75abace49631
Subham-u <subhammahapatra004@gmail.com>
2024-05-13 23:41:35 +0530

    view more button funtion

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -185,7 +185,8 @@ function Home() {
               />
             </div>
           </div>
-          <Button text="View More" color="mygreen" hover="myred" />
+          <Link to="/home/shop"><Button text="View More" color="mygreen" hover="myred" />
+          </Link>
         </div>
       </div>
 
