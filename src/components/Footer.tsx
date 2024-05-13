a8f569e0955e81792fe3ebd12336a01c77e2ab97
Srishtiii19 <srishtiiii19@gmail.com>
2024-05-13 17:10:38 +0530

    footer-navigation

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -30,7 +30,7 @@ function Footer() {
                             <ul className="mt-8 flex gap-6">
                                 <li>
                                     <a
-                                        href="#"
+                                        href="https://www.facebook.com/"
                                         rel="noreferrer"
                                         target="_blank"
                                         className="text-gray-700 transition hover:opacity-75"
@@ -49,7 +49,7 @@ function Footer() {
 
                                 <li>
                                     <a
-                                        href="#"
+                                        href="https://www.instagram.com/"
                                         rel="noreferrer"
                                         target="_blank"
                                         className="text-gray-700 transition hover:opacity-75"
@@ -68,7 +68,7 @@ function Footer() {
 
                                 <li>
                                     <a
-                                        href="#"
+                                        href="https://twitter.com/?lang=en"
                                         rel="noreferrer"
                                         target="_blank"
                                         className="text-gray-700 transition hover:opacity-75"
@@ -85,7 +85,7 @@ function Footer() {
 
                                 <li>
                                     <a
-                                        href="#"
+                                        href="https://github.com/"
                                         rel="noreferrer"
                                         target="_blank"
                                         className="text-gray-700 transition hover:opacity-75"
@@ -98,13 +98,14 @@ function Footer() {
                                                 d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z"
                                                 clipRule="evenodd"
                                             />
+
                                         </svg>
                                     </a>
                                 </li>
 
                                 <li>
                                     <a
-                                        href="#"
+                                        href="https://dribbble.com/"
                                         rel="noreferrer"
                                         target="_blank"
                                         className="text-gray-700 transition hover:opacity-75"
@@ -128,11 +129,11 @@ function Footer() {
                                 <p className="font-medium text-gray-900">Services</p>
 
                                 <ul className="mt-6 space-y-4 text-sm">
-                                    <li><Link to="/" className="text-gray-700 transition hover:opacity-75"> Profile </Link></li>
-                                    <li><Link to="/cart" className="text-gray-700 transition hover:opacity-75"> View Cart </Link></li>
-                                    <li><Link to="/shop" className="text-gray-700 transition hover:opacity-75"> Order Now</Link></li>
-                                    <li><Link to="#" className="text-gray-700 transition hover:opacity-75"> Join Us </Link></li>
-                                    <li><Link to="/#about" className="text-gray-700 transition hover:opacity-75">About</Link></li>
+                                    <li><Link to="/#" className="text-gray-700 transition hover:opacity-75"> Profile </Link></li>
+                                    <li><Link to="/home/cart" className="text-gray-700 transition hover:opacity-75"> View Cart </Link></li>
+                                    <li><Link to="/home/cart" className="text-gray-700 transition hover:opacity-75"> Order Now</Link></li>
+                                    <li><Link to="/#" className="text-gray-700 transition hover:opacity-75"> Join Us </Link></li>
+                                    <li><Link to="/home" className="text-gray-700 transition hover:opacity-75">About</Link></li>
                                 </ul>
                             </div>
                         </div>
