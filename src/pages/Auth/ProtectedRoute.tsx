c26fd0dc4f987fb1362b207251455595f0156bf5
Shashank <shashanksinghnegi12@gmail.com>
2024-05-13 21:07:40 +0530

    resolved dependency

--- src/pages/Auth/ProtectedRoute.tsx
+++ src/pages/Auth/ProtectedRoute.tsx
@@ -1,14 +1,15 @@
 import { useSelector } from "react-redux";
 import { RootState } from "../../utils/features/store";
-import Error from "../Error";
+import { useNavigate } from "react-router-dom";
 interface ProtectedRouteProps {
   children: React.ReactNode;
 }
 
 export default function ProtectedRoute({ children }: ProtectedRouteProps) {
   const isAuthenticated = useSelector((state: RootState) => state.auth.isAuthenticated);
+  const navigate = useNavigate();
    
-  return isAuthenticated !== undefined ? (isAuthenticated ? children : <Error/>) : null; // Conditional rendering based on state availability
+  return isAuthenticated !== undefined ? (isAuthenticated ? children : <button onClick={()=>{navigate('/')}}>Go to Login Page</button>) : null; // Conditional rendering based on state availability
   
 
 }
