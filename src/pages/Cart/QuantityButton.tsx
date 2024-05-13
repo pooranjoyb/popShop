5867f1c2fdafb3350b3b0f73b39f51899c54611a
Disha Hati <dishahati55@gmail.com>
2024-05-13 21:41:16 +0530

    Fixed Quantity Button

---
+++ src/pages/Cart/QuantityButton.tsx
@@ -0,0 +1,47 @@
+import React, { useState } from 'react'
+
+
+const QuantityButton = () => {
+    const [quantity, setQuantity] = useState(0);
+
+  const setIncrease = () => {
+    setQuantity((prevQuantity) => prevQuantity + 1);
+  };
+
+  const setDecrease = () => {
+    if (quantity > 0) {
+      setQuantity((prevQuantity) => prevQuantity - 1);
+    }
+  };
+  return (
+    <div className="inline-flex items-center px-4 font-semibold text-gray-500 border border-gray-200 rounded-md dark:border-gray-700 ">
+    <button onClick={() => setDecrease()} className="py-2 hover:text-gray-700 dark:text-gray-400">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          fill="currentColor"
+          className="bi bi-dash"
+          viewBox="0 0 16 16"
+        >
+          <path d="M4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8z"></path>
+        </svg>
+      </button>
+      <div className="w-12 px-2 py-4 text-center border-0 rounded-md dark:bg-gray-800 bg-gray-50 dark:text-gray-400 md:text-right">{quantity}</div>
+      <button onClick={()=>setIncrease()} className="py-2 hover:text-gray-700 dark:text-gray-400">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          fill="currentColor"
+          className="bi bi-plus"
+          viewBox="0 0 16 16"
+        >
+          <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"></path>
+        </svg>
+      </button>
+    </div>
+  )
+}
+
+export default QuantityButton
--- src/pages/Shop/ProductDetail.tsx
+++ src/pages/Shop/ProductDetail.tsx
@@ -4,6 +4,7 @@ import { useLocation } from "react-router-dom";
 // components
 import Head from "../../components/Head";
 import Button from "../../components/Button";
+import QuantityButton from "../Cart/QuantityButton";
 
 interface Data {
     name: string;
@@ -79,19 +80,7 @@ function ProductDetail() {
                                 <div className="w-32 mb-8 ">
                                     <label htmlFor=""
                                         className="w-full text-xl font-semibold text-gray-700 dark:text-gray-400">Quantity</label>
-                                    <div className="relative flex flex-row w-full h-10 mt-4 bg-transparent rounded-lg">
-                                        <button
-                                            className="w-20 h-full text-gray-600 bg-gray-300 rounded-l outline-none cursor-pointer dark:hover:bg-gray-700 dark:text-gray-400 hover:text-gray-700 dark:bg-gray-900 hover:bg-gray-400">
-                                            <span className="m-auto text-2xl font-thin">-</span>
-                                        </button>
-                                        <input type="number"
-                                            className="flex items-center w-full font-semibold text-center text-gray-700 placeholder-gray-700 bg-gray-300 outline-none dark:text-gray-400 dark:placeholder-gray-400 dark:bg-gray-900 focus:outline-none text-md hover:text-black"
-                                            placeholder="1" />
-                                        <button
-                                            className="w-20 h-full text-gray-600 bg-gray-300 rounded-r outline-none cursor-pointer dark:hover:bg-gray-700 dark:text-gray-400 dark:bg-gray-900 hover:text-gray-700 hover:bg-gray-400">
-                                            <span className="m-auto text-2xl font-thin">+</span>
-                                        </button>
-                                    </div>
+                                   <QuantityButton/>
                                 </div>
                                 <div className="flex flex-wrap items-center gap-10 ">
                                     
