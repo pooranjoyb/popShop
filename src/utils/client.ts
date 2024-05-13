4cd67f37da654cf4013553d496ad974a2fb1c131
yashmandi <yashmandi18@gmail.com>
2024-05-13 18:58:20 +0530

    updated changes

--- src/utils/client.ts
+++ src/utils/client.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://drnetvgmyxknfbeftdhy.supabase.co"
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRybmV0dmdteXhrbmZiZWZ0ZGh5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTAwOTM5OTcsImV4cCI6MjAyNTY2OTk5N30.5oi3T2rvQLdc5hBB-dY_EYEHlQ4VlVWplAGHQn_bir4"
+const supabaseUrl = import.meta.env.VITE_PROJECT_KEY
+const supabaseAnonKey = import.meta.env.VITE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
