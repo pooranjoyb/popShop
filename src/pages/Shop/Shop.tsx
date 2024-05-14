7d23e2b19d50d0072152ba82b5750c69971be299
Niharika Goulikar <goulikarniharika@gmail.com>
2024-05-14 16:52:15 +0530

    Initial set up

--- src/pages/Shop/Shop.tsx
+++ src/pages/Shop/Shop.tsx
@@ -1,14 +1,71 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 // components
 import Head from "../../components/Head";
 import Product from "../../components/Product";
-
+const data = [
+    {
+      desc: "",
+      image: "https://images.unsplash.com/photo-1578996953841-b187dbe4bc8a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mzl8fGJsYXplcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+      price: 20,
+      name: "Diamond Blue Suit"
+    },
+    {
+      desc: "",
+      image: "https://img.freepik.com/free-photo/model-suit-posing-studio_1303-12436.jpg?w=360&t=st=1708162653~exp=1708163253~hmac=04c46ae61b01bae7e68c3a3a52e084371b7529e79d0266419944786f5726b30b",
+      price: 99,
+      name: "Nomad Outfit"
+    },
+    {
+      desc: "",
+      image: "https://img.freepik.com/free-photo/full-shot-woman-posing-with-green-outfit_23-2150728960.jpg?w=360&t=st=1708162804~exp=1708163404~hmac=b8eed76bcc3e16b01902ab4b5993eacc511b701a7c8a21690277d73429c02907",
+      price: 56,
+      name: "Leaf Green Outfit"
+    },
+    {
+      desc: "",
+      image: "https://img.freepik.com/free-photo/full-shot-beautiful-lady_23-2148440576.jpg?w=360&t=st=1708162622~exp=1708163222~hmac=a0eb19d754c23e36840605ecb684ece75e8b20e74ba329c1d0958a10dec2824f",
+      price: 99,
+      name: "Red Casual Wear"
+    },
+    {
+      desc: "",
+      image: "https://images.unsplash.com/photo-1578996953841-b187dbe4bc8a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mzl8fGJsYXplcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+      price: 20,
+      name: "Diamond Blue Suit"
+    },
+    {
+      desc: "",
+      image: "https://img.freepik.com/free-photo/model-suit-posing-studio_1303-12436.jpg?w=360&t=st=1708162653~exp=1708163253~hmac=04c46ae61b01bae7e68c3a3a52e084371b7529e79d0266419944786f5726b30b",
+      price: 99,
+      name: "Nomad Outfit"
+    },
+    {
+      desc: "",
+      image: "https://img.freepik.com/free-photo/full-shot-woman-posing-with-green-outfit_23-2150728960.jpg?w=360&t=st=1708162804~exp=1708163404~hmac=b8eed76bcc3e16b01902ab4b5993eacc511b701a7c8a21690277d73429c02907",
+      price: 56,
+      name: "Leaf Green Outfit"
+    },
+    {
+      desc: "",
+      image: "https://img.freepik.com/free-photo/full-shot-beautiful-lady_23-2148440576.jpg?w=360&t=st=1708162622~exp=1708163222~hmac=a0eb19d754c23e36840605ecb684ece75e8b20e74ba329c1d0958a10dec2824f",
+      price: 99,
+      name: "Red Casual Wear"
+    }
+  ];
+  
+  
 function Shop() {
+    const [products,setproducts]=useState(data);
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
-
+  const handlesearch=(e:any)=>{
+    const val=e.target.value;
+    console.log(val,"hello")
+  const filtereddata=  data.filter((elem)=>{if(elem.name.includes(val))return true;else return false});
+  setproducts(filtereddata);
+   }
     useEffect(() => {
         window.scrollTo(0, 0);
     }, []);
@@ -19,7 +76,7 @@ function Shop() {
                 <Head h1="Our" h2="Store" />
                 <div>
                 <label className="input input-bordered input-success flex items-center gap-2">
-                    <input type="text" className="grow" placeholder="Search" />
+                    <input type="text" className="grow" placeholder="Search" onChange={handlesearch}/>
                     <svg
                     xmlns="http://www.w3.org/2000/svg"
                     viewBox="0 0 16 16"
@@ -37,18 +94,13 @@ function Shop() {
             </div>
             <div className="mx-auto max-w-2xl px-4 py-8 lg:max-w-7xl lg:px-8">
                 <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-                    <Product desc="" image="https://images.unsplash.com/photo-1578996953841-b187dbe4bc8a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mzl8fGJsYXplcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" price={20} name="Diamond Blue Suit" />
-                    <Product desc="" image="https://img.freepik.com/free-photo/model-suit-posing-studio_1303-12436.jpg?w=360&t=st=1708162653~exp=1708163253~hmac=04c46ae61b01bae7e68c3a3a52e084371b7529e79d0266419944786f5726b30b" price={99} name="Nomad Outfit" />
-                    <Product desc="" image="https://img.freepik.com/free-photo/full-shot-woman-posing-with-green-outfit_23-2150728960.jpg?w=360&t=st=1708162804~exp=1708163404~hmac=b8eed76bcc3e16b01902ab4b5993eacc511b701a7c8a21690277d73429c02907" price={56} name="Leaf Green Outfit" />
-                    <Product desc="" image="https://img.freepik.com/free-photo/full-shot-beautiful-lady_23-2148440576.jpg?w=360&t=st=1708162622~exp=1708163222~hmac=a0eb19d754c23e36840605ecb684ece75e8b20e74ba329c1d0958a10dec2824f" price={99} name="Red Casual Wear" />
-                    <Product desc="" image="https://images.unsplash.com/photo-1578996953841-b187dbe4bc8a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mzl8fGJsYXplcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" price={20} name="Diamond Blue Suit" />
-                    <Product desc="" image="https://img.freepik.com/free-photo/model-suit-posing-studio_1303-12436.jpg?w=360&t=st=1708162653~exp=1708163253~hmac=04c46ae61b01bae7e68c3a3a52e084371b7529e79d0266419944786f5726b30b" price={99} name="Nomad Outfit" />
-                    <Product desc="" image="https://img.freepik.com/free-photo/full-shot-woman-posing-with-green-outfit_23-2150728960.jpg?w=360&t=st=1708162804~exp=1708163404~hmac=b8eed76bcc3e16b01902ab4b5993eacc511b701a7c8a21690277d73429c02907" price={56} name="Leaf Green Outfit" />
-                    <Product desc="" image="https://img.freepik.com/free-photo/full-shot-beautiful-lady_23-2148440576.jpg?w=360&t=st=1708162622~exp=1708163222~hmac=a0eb19d754c23e36840605ecb684ece75e8b20e74ba329c1d0958a10dec2824f" price={99} name="Red Casual Wear" />
-                    <Product desc="" image="https://images.unsplash.com/photo-1578996953841-b187dbe4bc8a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mzl8fGJsYXplcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" price={20} name="Diamond Blue Suit" />
-                    <Product desc="" image="https://img.freepik.com/free-photo/model-suit-posing-studio_1303-12436.jpg?w=360&t=st=1708162653~exp=1708163253~hmac=04c46ae61b01bae7e68c3a3a52e084371b7529e79d0266419944786f5726b30b" price={99} name="Nomad Outfit" />
-                    <Product desc="" image="https://img.freepik.com/free-photo/full-shot-woman-posing-with-green-outfit_23-2150728960.jpg?w=360&t=st=1708162804~exp=1708163404~hmac=b8eed76bcc3e16b01902ab4b5993eacc511b701a7c8a21690277d73429c02907" price={56} name="Leaf Green Outfit" />
-                    <Product desc="" image="https://img.freepik.com/free-photo/full-shot-beautiful-lady_23-2148440576.jpg?w=360&t=st=1708162622~exp=1708163222~hmac=a0eb19d754c23e36840605ecb684ece75e8b20e74ba329c1d0958a10dec2824f" price={99} name="Red Casual Wear" />
+                    {products.map((elem,idx)=>{
+                        return(
+                        <Product  key={idx} desc={elem.desc} image={elem.image} price={elem.price} name={elem.name} />
+                        )
+                    })}
+                    
+              
                 </div>
             </div>
         </>
