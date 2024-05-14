d8fe6f5cc90d35eb7e1bec66cbece9cbaad54dfe
yashmandi <yashmandi18@gmail.com>
2024-05-14 10:02:02 +0530

    #55 added buy now button and cart icon on each product

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -1,29 +1,35 @@
 import { useNavigate } from "react-router-dom";
+import { ReactNode } from "react";
 
-interface Data {
+interface ProductProps {
     name: string;
     image: string;
     price: number;
     desc: string;
+    children?: ReactNode;
 }
 
-function Product({ name, image, price, desc }: Data) {
+function Product({ name, image, price, desc, children }: ProductProps) {
     const navigate = useNavigate();
 
-    const handleNavigate = ( ) => {
+    const handleNavigate = () => {
         navigate("/home/product", { state: { name, image, price, desc } });
     }
+
     return (
-        <>
+        <div>
             <button onClick={handleNavigate}>
                 <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
                     <img src={image} alt="Tall slender porcelain bottle with natural clay textured body and cork stopper." className="h-full w-full object-cover object-center group-hover:opacity-75 hover:duration-300" />
                 </div>
-                <h3 className="mt-4 text-sm text-gray-700">{name}</h3>
-                <p className="mt-1 text-lg font-medium text-gray-900">${price}</p>
+                <div>
+                    <h3 className="mt-4 text-sm text-gray-700 text-left">{name}</h3>
+                    <p className="mt-1 text-lg font-medium text-left text-gray-900">${price}</p>
+                    {children}
+                </div>
             </button>
-        </>
+        </div>
     )
 }
 
-export default Product
+export default Product;
--- src/pages/Shop/Shop.tsx
+++ src/pages/Shop/Shop.tsx
@@ -1,14 +1,24 @@
 import { useEffect } from "react"
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 // components
 import Head from "../../components/Head"
 import Product from "../../components/Product"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHeart, faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+import Button from "../../components/Button";
 
 function Shop() {
 
     useEffect(() => {
         window.scrollTo(0, 0);
     }, []);
+
+    const addToCart = () => {
+        // logic for adding in cart
+        toast.success('Added to Cart', { autoClose: 2000 });
+    };
 
     return (
         <>
@@ -22,21 +32,151 @@ function Shop() {
                 </div>
             </div>
             <div className="mx-auto max-w-2xl px-4 py-8 lg:max-w-7xl lg:px-8">
-                <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-                    <Product desc="" image="https://images.unsplash.com/photo-1578996953841-b187dbe4bc8a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mzl8fGJsYXplcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" price={20} name="Diamond Blue Suit" />
-                    <Product desc="" image="https://img.freepik.com/free-photo/model-suit-posing-studio_1303-12436.jpg?w=360&t=st=1708162653~exp=1708163253~hmac=04c46ae61b01bae7e68c3a3a52e084371b7529e79d0266419944786f5726b30b" price={99} name="Nomad Outfit" />
-                    <Product desc="" image="https://img.freepik.com/free-photo/full-shot-woman-posing-with-green-outfit_23-2150728960.jpg?w=360&t=st=1708162804~exp=1708163404~hmac=b8eed76bcc3e16b01902ab4b5993eacc511b701a7c8a21690277d73429c02907" price={56} name="Leaf Green Outfit" />
-                    <Product desc="" image="https://img.freepik.com/free-photo/full-shot-beautiful-lady_23-2148440576.jpg?w=360&t=st=1708162622~exp=1708163222~hmac=a0eb19d754c23e36840605ecb684ece75e8b20e74ba329c1d0958a10dec2824f" price={99} name="Red Casual Wear" />
-                    <Product desc="" image="https://images.unsplash.com/photo-1578996953841-b187dbe4bc8a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mzl8fGJsYXplcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" price={20} name="Diamond Blue Suit" />
-                    <Product desc="" image="https://img.freepik.com/free-photo/model-suit-posing-studio_1303-12436.jpg?w=360&t=st=1708162653~exp=1708163253~hmac=04c46ae61b01bae7e68c3a3a52e084371b7529e79d0266419944786f5726b30b" price={99} name="Nomad Outfit" />
-                    <Product desc="" image="https://img.freepik.com/free-photo/full-shot-woman-posing-with-green-outfit_23-2150728960.jpg?w=360&t=st=1708162804~exp=1708163404~hmac=b8eed76bcc3e16b01902ab4b5993eacc511b701a7c8a21690277d73429c02907" price={56} name="Leaf Green Outfit" />
-                    <Product desc="" image="https://img.freepik.com/free-photo/full-shot-beautiful-lady_23-2148440576.jpg?w=360&t=st=1708162622~exp=1708163222~hmac=a0eb19d754c23e36840605ecb684ece75e8b20e74ba329c1d0958a10dec2824f" price={99} name="Red Casual Wear" />
-                    <Product desc="" image="https://images.unsplash.com/photo-1578996953841-b187dbe4bc8a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mzl8fGJsYXplcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" price={20} name="Diamond Blue Suit" />
-                    <Product desc="" image="https://img.freepik.com/free-photo/model-suit-posing-studio_1303-12436.jpg?w=360&t=st=1708162653~exp=1708163253~hmac=04c46ae61b01bae7e68c3a3a52e084371b7529e79d0266419944786f5726b30b" price={99} name="Nomad Outfit" />
-                    <Product desc="" image="https://img.freepik.com/free-photo/full-shot-woman-posing-with-green-outfit_23-2150728960.jpg?w=360&t=st=1708162804~exp=1708163404~hmac=b8eed76bcc3e16b01902ab4b5993eacc511b701a7c8a21690277d73429c02907" price={56} name="Leaf Green Outfit" />
-                    <Product desc="" image="https://img.freepik.com/free-photo/full-shot-beautiful-lady_23-2148440576.jpg?w=360&t=st=1708162622~exp=1708163222~hmac=a0eb19d754c23e36840605ecb684ece75e8b20e74ba329c1d0958a10dec2824f" price={99} name="Red Casual Wear" />
-                </div>
-            </div>
+                <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8 ">
+                    <Product desc="" image="https://images.unsplash.com/photo-1578996953841-b187dbe4bc8a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mzl8fGJsYXplcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" price={20} name="Diamond Blue Suit">
+                        <div className="flex flex-row justify-between px-2 mt-2 ml-[-10px] ">
+                            <div className="flex flex-col ">
+                                <Button text="Buy Now" color="mygreen" hover="myred" />
+                            </div>
+                            <div className="flex flex-row gap-4 mt-1">
+                                <FontAwesomeIcon icon={faHeart} className="cursor-pointer h-9 w-8 transition-colors duration-300 hover:text-red-600" />
+                                <FontAwesomeIcon icon={faShoppingCart} className="cursor-pointer h-9 w-8 transition-colors duration-300 hover:text-blue-600" onClick={addToCart} />
+                            </div>
+                        </div>
+                    </Product>
+
+                    <Product desc="" image="https://img.freepik.com/free-photo/model-suit-posing-studio_1303-12436.jpg?w=360&t=st=1708162653~exp=1708163253~hmac=04c46ae61b01bae7e68c3a3a52e084371b7529e79d0266419944786f5726b30b" price={99} name="Nomad Outfit" > <div className="flex flex-row justify-between px-2 mt-2 ml-[-10px] ">
+                        <div className="flex flex-col ">
+                            <Button text="Buy Now" color="mygreen" hover="myred" />
+                        </div>
+                        <div className="flex flex-row gap-4 mt-1">
+                            <FontAwesomeIcon icon={faHeart} className="cursor-pointer h-9 w-8 transition-colors duration-300 hover:text-red-600" />
+                            <FontAwesomeIcon icon={faShoppingCart} className="cursor-pointer h-9 w-8 transition-colors duration-300 hover:text-blue-600" onClick={addToCart} />
+                        </div>
+                    </div>
+                    </Product>
+
+                    <Product desc="" image="https://img.freepik.com/free-photo/full-shot-woman-posing-with-green-outfit_23-2150728960.jpg?w=360&t=st=1708162804~exp=1708163404~hmac=b8eed76bcc3e16b01902ab4b5993eacc511b701a7c8a21690277d73429c02907" price={56} name="Leaf Green Outfit" >
+                        <div className="flex flex-row justify-between px-2 mt-2 ml-[-10px] ">
+                            <div className="flex flex-col ">
+                                <Button text="Buy Now" color="mygreen" hover="myred" />
+                            </div>
+                            <div className="flex flex-row gap-4 mt-1">
+                                <FontAwesomeIcon icon={faHeart} className="cursor-pointer h-9 w-8 transition-colors duration-300 hover:text-red-600" />
+                                <FontAwesomeIcon icon={faShoppingCart} className="cursor-pointer h-9 w-8 transition-colors duration-300 hover:text-blue-600" onClick={addToCart} />
+                            </div>
+                        </div>
+                    </Product>
+
+                    <Product desc="" image="https://img.freepik.com/free-photo/full-shot-beautiful-lady_23-2148440576.jpg?w=360&t=st=1708162622~exp=1708163222~hmac=a0eb19d754c23e36840605ecb684ece75e8b20e74ba329c1d0958a10dec2824f" price={99} name="Red Casual Wear" >
+                        <div className="flex flex-row justify-between px-2 mt-2 ml-[-10px] ">
+                            <div className="flex flex-col ">
+                                <Button text="Buy Now" color="mygreen" hover="myred" />
+                            </div>
+                            <div className="flex flex-row gap-4 mt-1">
+                                <FontAwesomeIcon icon={faHeart} className="cursor-pointer h-9 w-8 transition-colors duration-300 hover:text-red-600" />
+                                <FontAwesomeIcon icon={faShoppingCart} className="cursor-pointer h-9 w-8 transition-colors duration-300 hover:text-blue-600" onClick={addToCart} />
+                            </div>
+                        </div>
+                    </Product>
+
+                    <Product desc="" image="https://images.unsplash.com/photo-1578996953841-b187dbe4bc8a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mzl8fGJsYXplcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" price={20} name="Diamond Blue Suit" >
+                        <div className="flex flex-row justify-between px-2 mt-2 ml-[-10px] ">
+                            <div className="flex flex-col ">
+                                <Button text="Buy Now" color="mygreen" hover="myred" />
+                            </div>
+                            <div className="flex flex-row gap-4 mt-1">
+                                <FontAwesomeIcon icon={faHeart} className="cursor-pointer h-9 w-8 transition-colors duration-300 hover:text-red-600" />
+                                <FontAwesomeIcon icon={faShoppingCart} className="cursor-pointer h-9 w-8 transition-colors duration-300 hover:text-blue-600" onClick={addToCart} />
+                            </div>
+                        </div>
+                    </Product>
+
+                    <Product desc="" image="https://img.freepik.com/free-photo/model-suit-posing-studio_1303-12436.jpg?w=360&t=st=1708162653~exp=1708163253~hmac=04c46ae61b01bae7e68c3a3a52e084371b7529e79d0266419944786f5726b30b" price={99} name="Nomad Outfit" >
+                        <div className="flex flex-row justify-between px-2 mt-2 ml-[-10px] ">
+                            <div className="flex flex-col ">
+                                <Button text="Buy Now" color="mygreen" hover="myred" />
+                            </div>
+                            <div className="flex flex-row gap-4 mt-1">
+                                <FontAwesomeIcon icon={faHeart} className="cursor-pointer h-9 w-8 transition-colors duration-300 hover:text-red-600" />
+                                <FontAwesomeIcon icon={faShoppingCart} className="cursor-pointer h-9 w-8 transition-colors duration-300 hover:text-blue-600" onClick={addToCart} />
+                            </div>
+                        </div>
+                    </Product>
+
+                    <Product desc="" image="https://img.freepik.com/free-photo/full-shot-woman-posing-with-green-outfit_23-2150728960.jpg?w=360&t=st=1708162804~exp=1708163404~hmac=b8eed76bcc3e16b01902ab4b5993eacc511b701a7c8a21690277d73429c02907" price={56} name="Leaf Green Outfit" >
+                        <div className="flex flex-row justify-between px-2 mt-2 ml-[-10px] ">
+                            <div className="flex flex-col ">
+                                <Button text="Buy Now" color="mygreen" hover="myred" />
+                            </div>
+                            <div className="flex flex-row gap-4 mt-1">
+                                <FontAwesomeIcon icon={faHeart} className="cursor-pointer h-9 w-8 transition-colors duration-300 hover:text-red-600" />
+                                <FontAwesomeIcon icon={faShoppingCart} className="cursor-pointer h-9 w-8 transition-colors duration-300 hover:text-blue-600" onClick={addToCart} />
+                            </div>
+                        </div>
+                    </Product>
+
+                    <Product desc="" image="https://img.freepik.com/free-photo/full-shot-beautiful-lady_23-2148440576.jpg?w=360&t=st=1708162622~exp=1708163222~hmac=a0eb19d754c23e36840605ecb684ece75e8b20e74ba329c1d0958a10dec2824f" price={99} name="Red Casual Wear" >
+                        <div className="flex flex-row justify-between px-2 mt-2 ml-[-10px] ">
+                            <div className="flex flex-col ">
+                                <Button text="Buy Now" color="mygreen" hover="myred" />
+                            </div>
+                            <div className="flex flex-row gap-4 mt-1">
+                                <FontAwesomeIcon icon={faHeart} className="cursor-pointer h-9 w-8 transition-colors duration-300 hover:text-red-600" />
+                                <FontAwesomeIcon icon={faShoppingCart} className="cursor-pointer h-9 w-8 transition-colors duration-300 hover:text-blue-600" onClick={addToCart} />
+                            </div>
+                        </div>
+                    </Product>
+
+                    <Product desc="" image="https://images.unsplash.com/photo-1578996953841-b187dbe4bc8a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mzl8fGJsYXplcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" price={20} name="Diamond Blue Suit">
+                        <div className="flex flex-row justify-between px-2 mt-2 ml-[-10px] ">
+                            <div className="flex flex-col ">
+                                <Button text="Buy Now" color="mygreen" hover="myred" />
+                            </div>
+                            <div className="flex flex-row gap-4 mt-1">
+                                <FontAwesomeIcon icon={faHeart} className="cursor-pointer h-9 w-8 transition-colors duration-300 hover:text-red-600" />
+                                <FontAwesomeIcon icon={faShoppingCart} className="cursor-pointer h-9 w-8 transition-colors duration-300 hover:text-blue-600" onClick={addToCart} />
+                            </div>
+                        </div>
+                    </Product>
+
+                    <Product desc="" image="https://img.freepik.com/free-photo/model-suit-posing-studio_1303-12436.jpg?w=360&t=st=1708162653~exp=1708163253~hmac=04c46ae61b01bae7e68c3a3a52e084371b7529e79d0266419944786f5726b30b" price={99} name="Nomad Outfit" >
+                        <div className="flex flex-row justify-between px-2 mt-2 ml-[-10px] ">
+                            <div className="flex flex-col ">
+                                <Button text="Buy Now" color="mygreen" hover="myred" />
+                            </div>
+                            <div className="flex flex-row gap-4 mt-1">
+                                <FontAwesomeIcon icon={faHeart} className="cursor-pointer h-9 w-8 transition-colors duration-300 hover:text-red-600" />
+                                <FontAwesomeIcon icon={faShoppingCart} className="cursor-pointer h-9 w-8 transition-colors duration-300 hover:text-blue-600" onClick={addToCart} />
+                            </div>
+                        </div>
+                    </Product>
+
+                    <Product desc="" image="https://img.freepik.com/free-photo/full-shot-woman-posing-with-green-outfit_23-2150728960.jpg?w=360&t=st=1708162804~exp=1708163404~hmac=b8eed76bcc3e16b01902ab4b5993eacc511b701a7c8a21690277d73429c02907" price={56} name="Leaf Green Outfit" >
+                        <div className="flex flex-row justify-between px-2 mt-2 ml-[-10px] ">
+                            <div className="flex flex-col ">
+                                <Button text="Buy Now" color="mygreen" hover="myred" />
+                            </div>
+                            <div className="flex flex-row gap-4 mt-1">
+                                <FontAwesomeIcon icon={faHeart} className="cursor-pointer h-9 w-8 transition-colors duration-300 hover:text-red-600" />
+                                <FontAwesomeIcon icon={faShoppingCart} className="cursor-pointer h-9 w-8 transition-colors duration-300 hover:text-blue-600" onClick={addToCart} />
+                            </div>
+                        </div>
+                    </Product>
+
+                    <Product desc="" image="https://img.freepik.com/free-photo/full-shot-beautiful-lady_23-2148440576.jpg?w=360&t=st=1708162622~exp=1708163222~hmac=a0eb19d754c23e36840605ecb684ece75e8b20e74ba329c1d0958a10dec2824f" price={99} name="Red Casual Wear">
+                        <div className="flex flex-row justify-between px-2 mt-2 ml-[-10px] ">
+                            <div className="flex flex-col ">
+                                <Button text="Buy Now" color="mygreen" hover="myred" />
+                            </div>
+                            <div className="flex flex-row gap-4 mt-1">
+                                <FontAwesomeIcon icon={faHeart} className="cursor-pointer h-9 w-8 transition-colors duration-300 hover:text-red-600" />
+                                <FontAwesomeIcon icon={faShoppingCart} className="cursor-pointer h-9 w-8 transition-colors duration-300 hover:text-blue-600" onClick={addToCart} />
+                            </div>
+                        </div>
+                    </Product>
+                </div >
+            </div >
         </>
     )
 }
