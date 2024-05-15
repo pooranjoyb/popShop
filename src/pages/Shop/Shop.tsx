1f79a3e43f18b43bccc99c9d927f0f3bee448abd
Tomkndn <ntamsoy@gmail.com>
2024-05-16 04:06:28 +0530

    feat-adding filtering and sorting option

--- src/pages/Shop/Shop.tsx
+++ src/pages/Shop/Shop.tsx
@@ -10,13 +10,60 @@ function Shop() {
         window.scrollTo(0, 0);
     }, []);
 
+    const filter = [
+        {
+        filterOption: 'Size',
+        checkbox: ['XS', 'S', 'M', 'L', 'XL']
+        },
+        {
+        filterOption: 'Price Range',
+        checkbox: ["below-₹1000", "₹1000-₹2000", "₹2000-₹4000", "₹4000-₹8000", "₹8000-and above"]
+        },
+    ]
 
     return (
         <>
             <div className=" mx-auto max-w-screen-xl px-4 py-12 flex justify-between items-center">
                 <Head h1="Our" h2="Store" />
-                <div>
-                    <label className="input input-bordered input-success flex items-center gap-2">
+                <div className="flex gap-6">
+                {/* The button to open modal */}
+                    <label htmlFor="my_modal_6" className="btn bg-mygreen hover:bg-myyellow">Filters</label>
+                    {/* Modal Body*/}
+                    <input type="checkbox" id="my_modal_6" className="modal-toggle w-5xl" />
+                    <div className="modal" role="dialog">
+                        <div className="modal-box w-[30rem]">
+                            <h3 className="font-bold text-center text-lg">Apply your filters</h3>
+                            <div className="flex font-semiold justify-around mt-5">
+                                {filter.map((fil,idx) => 
+                                    <div key={idx}>
+                                        <h2 className="mb-3">{fil.filterOption}</h2>
+                                        {fil.checkbox.map((size,index) => {return (
+                                            <div key={index} className="flex items-center mb-2">
+                                            <input
+                                              id={`default-checkbox${idx}${index}`}
+                                              type="checkbox"
+                                              value=""
+                                              className="w-4 h-4 checkbox"
+                                            />
+                                            <label
+                                              htmlFor={`default-checkbox${idx}${index}`}
+                                              className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                                            >
+                                              {size}
+                                            </label>
+                                          </div>
+                                        );})}
+                                </div>
+                                )}
+                            </div>
+                            
+                            <div className="modal-action justify-center pe-5">
+                                <label htmlFor="my_modal_6" className="btn hover:bg-mygreen bg-myyellow">Apply</label>
+                            </div>
+                        </div>
+                    </div>
+                
+                    <label className="input input-bordered input-success flex items-center gap-2 w-64">
                         <input type="text" className="grow" placeholder="Search" />
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path fillRule="evenodd" d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z" clipRule="evenodd" /></svg>
                     </label>
