b9515b96d666964b972a8e23638c8ccb0bc16e5e
Siddharth Bramhecha <101277141+Sid-80@users.noreply.github.com>
2024-05-17 13:07:15 +0000

    fix-image

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -1,7 +1,6 @@
 import { useSelector } from "react-redux";
 import { RootState } from "../../utils/features/store";
 import { Link, useNavigate } from "react-router-dom";
-import { FaUser } from "react-icons/fa6";
 import Head from "../../components/Head";
 
 function Profile() {
@@ -16,10 +15,9 @@ function Profile() {
 
     return (
         <div className="flex relative flex-col gap-5 items-center justify-center px-10 lg:px-[300px]">
-            <img className="absolute opacity-30 -z-20 w-full h-[220px] sm:h-[350px] top-[-98px]" src="/images/backbest.jpeg" alt="" />
-            <div className="divider divider-neutral text-lg font-semibold sm:text-3xl divider-start">
-                <FaUser width={20} />
-                Profile
+            <div className="absolute profile-background -z-20 w-full h-[250px] sm:h-[350px] top-[-98px]"></div>
+            <div className="sm:flex py-6 sm:py-0 sm:text-start w-full">
+                <Head h1="Profile" h2="" />
             </div>
             <div className="avatar">
                 <div className="w-24 sm:w-80 rounded-full">
@@ -27,7 +25,7 @@ function Profile() {
                 </div>
             </div>
             <div className="w-full text-center">
-                <Head nextLine={true} h1={username!} h2="First Name Last Name" />
+                <Head h1={username!} h2="" />
             </div>
             <Link
                 to="/home/shop"
@@ -41,19 +39,25 @@ function Profile() {
                     <div className="flex-1 lg:p-12 text-justify">
                         <div className="flex sm:flex-row flex-col items-start sm:items-center  text-lg sm:text-xl justify-start">
                             <div className="label">
-                                <span className="label-text text-xl">Gender : </span>
+                                <span className="label-text text-xl">First Name : </span>
                             </div>
-                            <h1 className=" font-bold text-mynavy">Male</h1>
+                            <h1 className=" font-bold text-mynavy">Fname</h1>
                         </div>
                         <div className="flex sm:flex-row flex-col items-start sm:items-center  text-lg sm:text-xl justify-start">
                             <div className="label">
-                                <span className="label-text text-xl">Account Creation Date : </span>
+                                <span className="label-text text-xl">Last Name : </span>
                             </div>
-                            <h1 className=" font-bold text-mynavy">1/1/24</h1>
+                            <h1 className=" font-bold text-mynavy">Lname</h1>
+                        </div>
+                        <div className="flex sm:flex-row flex-col items-start sm:items-center  text-lg sm:text-xl justify-start">
+                            <div className="label">
+                                <span className="label-text text-xl">Gender</span>
+                            </div>
+                            <h1 className=" font-bold text-mynavy">Male</h1>
                         </div>
                     </div>
 
-                    <div className="hidden sm:block border border-l-[1px]" />
+                    <div className="hidden sm:block border-gray-400 border-l-[2px]" />
 
                     <div className="flex-1 lg:p-12 text-justify">
                         <div className="flex sm:flex-row flex-col items-start sm:items-center  text-lg sm:text-xl justify-start">
@@ -61,12 +65,18 @@ function Profile() {
                                 <span className="label-text text-xl">Address : </span>
                             </div>
                             <h1 className=" font-bold text-mynavy">Address</h1>
-                        </div>
+                        </div>   
                         <div className="flex sm:flex-row flex-col items-start sm:items-center text-lg sm:text-xl justify-start">
                             <div className="label">
                                 <span className="label-text text-xl">Phone Number : </span>
                             </div>
                             <h1 className=" font-bold text-mynavy">1234567890</h1>
+                        </div>
+                        <div className="flex sm:flex-row flex-col items-start sm:items-center  text-lg sm:text-xl justify-start">
+                            <div className="label">
+                                <span className="label-text text-xl">Account Creation Date : </span>
+                            </div>
+                            <h1 className=" font-bold text-mynavy">1/1/24</h1>
                         </div>
                     </div>
                 </div>
