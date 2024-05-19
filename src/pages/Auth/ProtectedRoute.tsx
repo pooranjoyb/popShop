87fe045dc614dc5e7e351f27fec1c7d4fd894bf0
Mayur <mayursonare18@gmail.com>
2024-05-19 18:58:13 +0530

    feat-#82: Refining Protected Routes.

--- src/pages/Auth/ProtectedRoute.tsx
+++ src/pages/Auth/ProtectedRoute.tsx
@@ -1,15 +1,13 @@
 import { useSelector } from "react-redux";
 import { RootState } from "../../utils/features/store";
-import { useNavigate } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 interface ProtectedRouteProps {
   children: React.ReactNode;
 }
 
 export default function ProtectedRoute({ children }: ProtectedRouteProps) {
   const isAuthenticated = useSelector((state: RootState) => state.auth?.isAuthenticated);
-  const navigate = useNavigate();
    
-  return isAuthenticated !== undefined ? (isAuthenticated ? children : <button onClick={()=>{navigate('/')}}>Go to Login Page</button>) : null; // Conditional rendering based on state availability
+  return isAuthenticated !== undefined ? (isAuthenticated ? children : <Navigate to="/" />): null; // Conditional rendering based on state availability
   
-
-}
+  }
