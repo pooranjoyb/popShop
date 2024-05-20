4503ceb713d2276b9057963301dccd6a8b9815ce
shashank <shashanksinghnegi12@gmail.com>
2024-05-20 13:56:12 +0530

    resolved routes

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import { Provider } from 'react-redux'
 import store from './utils/features/store.ts'
 import { PersistGate } from 'redux-persist/integration/react'
 import { persistStore } from 'redux-persist'
-import { GoogleOAuthProvider } from '@react-oauth/google';
+
 
 
 const persistor = persistStore(store);
@@ -16,10 +16,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <PersistGate persistor={persistor}>
     <Provider store={store}>
-      <GoogleOAuthProvider clientId="590956334750-mtmvisc4r6tk62f1v6j37ajb8ve5u8ib.apps.googleusercontent.com" >
       <App />
-      </GoogleOAuthProvider>
-    
     </Provider>
     </PersistGate>
   </React.StrictMode>,
