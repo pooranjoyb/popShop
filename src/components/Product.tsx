a6be1470c4bbb5404963395ed4d1e5e06f35f7a3
Amit kumar <sidhant4688@gmail.com>
2024-05-26 02:33:45 +0530

    added buttons on Products page

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -1,4 +1,6 @@
 import { useNavigate } from "react-router-dom";
+import Button from "./Button";
+import { toast } from 'react-toastify';
 
 interface Data {
   name: string;
@@ -9,23 +11,34 @@ interface Data {
 
 function Product({ name, image, price, desc }: Data) {
   const navigate = useNavigate();
-
+  const addToCart = () => {
+    // logic for adding in cart
+    toast.success('Added to Cart', { autoClose: 2000 });
+  };
   const handleNavigate = () => {
     navigate("/home/product", { state: { name, image, price, desc } });
   };
   return (
     <>
-      <button onClick={handleNavigate}>
-        <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
-          <img
-            src={image}
-            alt="Tall slender porcelain bottle with natural clay textured body and cork stopper."
-            className="h-full w-full object-cover object-center group-hover:opacity-75 hover:duration-300"
-          />
+      <div className="flex flex-col">
+        <button onClick={handleNavigate}>
+          <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
+            <img
+              src={image}
+              alt="Tall slender porcelain bottle with natural clay textured body and cork stopper."
+              className="h-full w-full object-cover object-center group-hover:opacity-75 hover:duration-300"
+            />
+          </div>
+          <h3 className="mt-4 text-sm text-gray-700">{name}</h3>
+          <p className="mt-1 text-lg font-medium text-gray-900">${price}</p>
+        </button>
+        <div className="flex flex-auto justify-between p-8 space-x-24">
+
+          <Button text="Add to Cart" color="mygreen" hover="myred" onClick={addToCart} />
+          <Button text="Buy Now" color="myyellow" hover="myred" />
         </div>
-        <h3 className="mt-4 text-sm text-gray-700">{name}</h3>
-        <p className="mt-1 text-lg font-medium text-gray-900">${price}</p>
-      </button>
+      </div>
+      
     </>
   );
 }
