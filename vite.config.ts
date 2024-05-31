d6a8c5c697389c23237e19afe8ca42a0bc2fcda8
Shweta Meena <119609866+me-shweta@users.noreply.github.com>
2024-05-31 10:45:21 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          // Define manual chunks based on module ID
-          if (id.includes('node_modules')) {
-            // Place node_modules dependencies into a separate chunk
-            return 'vendor';
-          }
-        },
-      },
-    },
-    chunkSizeWarningLimit: 1000, // Increase the limit from 500 kB to 1000 kB
-  },
-});
+})
