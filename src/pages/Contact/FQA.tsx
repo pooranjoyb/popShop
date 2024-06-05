5a5d04248d8943834f77f41662d1de2ff4f21b2e
sambhav kaushik <masterboy376@gmail.com>
2024-06-05 13:52:49 +0530

    Implemented contact feature and added FQA section.

---
+++ src/pages/Contact/FQA.tsx
@@ -0,0 +1,72 @@
+import Head from "../../components/Head";
+
+function FQA() {
+    return (
+        <>
+            <div
+                className="relative w-full bg-white px-6 pt-10 pb-8 mt-8 sm:mx-auto sm:max-w-2xl sm:rounded-lg sm:px-10">
+                <div className="mx-auto px-5">
+                    <div className="flex flex-col items-center">
+                        <Head h1="Explore" h2="FAQs" />
+                    </div>
+                    <div className="mx-auto mt-8 grid max-w-xl divide-y divide-neutral-200">
+                        <div className="py-5">
+                            <details className="group">
+                                <summary className="flex cursor-pointer list-none items-center justify-between font-medium">
+                                    <span className="text-mynavy"> How does the billing work?</span>
+                                    <span className="transition group-open:rotate-180">
+                                        <svg fill="none" height="24" shape-rendering="geometricPrecision"
+                                            stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                            stroke-width="1.5" viewBox="0 0 24 24" width="24">
+                                            <path d="M6 9l6 6 6-6"></path>
+                                        </svg>
+                                    </span>
+                                </summary>
+                                <p className="group-open:animate-fadeIn mt-3 text-mynavy">PopShop offers a variety of
+                                    billing options, including net banking, cash on delivery and UPI.
+                                </p>
+                            </details>
+                        </div>
+                        <div className="py-5">
+                            <details className="group">
+                                <summary className="flex cursor-pointer list-none items-center justify-between font-medium">
+                                    <span className="text-mynavy"> Can I get a refund?</span>
+                                    <span className="transition group-open:rotate-180">
+                                        <svg fill="none" height="24" shape-rendering="geometricPrecision"
+                                            stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                            stroke-width="1.5" viewBox="0 0 24 24" width="24">
+                                            <path d="M6 9l6 6 6-6"></path>
+                                        </svg>
+                                    </span>
+                                </summary>
+                                <p className="group-open:animate-fadeIn mt-3 text-mynavy">We offer a 7-day money-back
+                                    guarantee for most of the orders. The refund process usually takes 2 to 3 working days.
+                                </p>
+                            </details>
+                        </div>
+                        <div className="py-5">
+                            <details className="group">
+                                <summary className="flex cursor-pointer list-none items-center justify-between font-medium">
+                                    <span className="text-mynavy"> How do I cancel my order?</span>
+                                    <span className="transition group-open:rotate-180">
+                                        <svg fill="none" height="24" shape-rendering="geometricPrecision"
+                                            stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                            stroke-width="1.5" viewBox="0 0 24 24" width="24">
+                                            <path d="M6 9l6 6 6-6"></path>
+                                        </svg>
+                                    </span>
+                                </summary>
+                                <p className="group-open:animate-fadeIn mt-3 text-mynavy">To cancel your order, you can
+                                    log in to your account and navigate to the your orders page. From there, you
+                                    should be able to cancel your order.
+                                </p>
+                            </details>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default FQA;
