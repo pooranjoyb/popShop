6adef52749a001499ef56cc9467008b433badc3d
Soni Dhruv <dhruvrsoni04@gmail.com>
2024-06-08 09:00:24 +0530

    update cart with actual items

--- src/pages/Cart/Cart.tsx
+++ src/pages/Cart/Cart.tsx
@@ -1,11 +1,23 @@
-import { useEffect } from "react";
+import React, { useEffect } from 'react';
+import { useSelector } from 'react-redux';
 
 // components
-import Head from "../../components/Head";
-import Button from "../../components/Button";
-import QuantityButton from "./QuantityButton";
+import Head from '../../components/Head';
+import Button from '../../components/Button';
+import QuantityButton from './QuantityButton';
+import { RootState } from '../../utils/features/store';
 
-function Cart() {
+export interface ITEM {
+  id: string;
+  description: string;
+  price: string;
+  title: string;
+  image: string; // Add this line
+}
+
+const Cart: React.FC = () => {
+  const cartItems = useSelector((state: RootState) => state.cart.item);
+
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
@@ -39,90 +51,49 @@ function Cart() {
                 </div>
                 <div className="hidden md:block px-4 text-right md:w-1/6 lg:w-2/12 ">
                   <h2 className="font-bold text-gray-500 dark:text-gray-400">
-                    {" "}
                     Subtotal
                   </h2>
                 </div>
               </div>
               <div className="py-4 mb-8 border-t border-b border-gray-200 dark:border-gray-700">
-                <div className="flex flex-wrap items-center mb-6 -mx-4 md:mb-8">
-                  <div className="w-full px-4 mb-6 md:w-4/6 lg:w-6/12 md:mb-0">
-                    <div className="flex flex-wrap items-center -mx-4">
-                      <div className="w-full px-4 mb-3 md:w-1/3">
-                        <div className="w-full h-96 md:h-24 md:w-24">
-                          <img
-                            src="https://i.postimg.cc/kGjz3dpD/pexels-cottonbro-3296434.jpg"
-                            alt=""
-                            className="object-cover w-full h-full"
-                          />
+                {cartItems.map((item: ITEM) => (
+                  <div key={item.id} className="flex flex-wrap items-center mb-6 -mx-4 md:mb-8">
+                    <div className="w-full px-4 mb-6 md:w-4/6 lg:w-6/12 md:mb-0">
+                      <div className="flex flex-wrap items-center -mx-4">
+                        <div className="w-full px-4 mb-3 md:w-1/3">
+                          <div className="w-full h-96 md:h-24 md:w-24">
+                            <img
+                              src={item.image}
+                              alt=""
+                              className="object-cover w-full h-full"
+                            />
+                          </div>
+                        </div>
+                        <div className="w-2/3 px-4">
+                          <h2 className="mb-2 text-xl font-bold dark:text-gray-400">
+                            {item.title}
+                          </h2>
+                          <p className="text-gray-500 dark:text-gray-400 ">
+                            {item.description}
+                          </p>
                         </div>
                       </div>
-                      <div className="w-2/3 px-4">
-                        <h2 className="mb-2 text-xl font-bold dark:text-gray-400">
-                          DSL Camera
-                        </h2>
-                        <p className="text-gray-500 dark:text-gray-400 ">
-                          Picture frame
-                        </p>
-                      </div>
+                    </div>
+                    <div className="hidden px-4 lg:block lg:w-2/12">
+                      <p className="text-lg font-bold text-blue-500 dark:text-gray-400">
+                        ${item.price}
+                      </p>
+                    </div>
+                    <div className="w-auto px-4 md:w-1/6 lg:w-2/12 ">
+                      <QuantityButton />
+                    </div>
+                    <div className="w-auto px-4 text-right md:w-1/6 lg:w-2/12 ">
+                      <p className="text-lg font-bold text-blue-500 dark:text-gray-400">
+                        ${item.price}
+                      </p>
                     </div>
                   </div>
-                  <div className="hidden px-4 lg:block lg:w-2/12">
-                    <p className="text-lg font-bold text-blue-500 dark:text-gray-400">
-                      $99.00
-                    </p>
-                    <span className="text-xs text-gray-500 line-through dark:text-gray-400">
-                      $1500
-                    </span>
-                  </div>
-                  <div className="w-auto px-4 md:w-1/6 lg:w-2/12 ">
-                    <QuantityButton/>
-                  </div>
-                  <div className="w-auto px-4 text-right md:w-1/6 lg:w-2/12 ">
-                    <p className="text-lg font-bold text-blue-500 dark:text-gray-400">
-                      $99.00
-                    </p>
-                  </div>
-                </div>
-                <div className="flex flex-wrap items-center mb-6 -mx-4 md:mb-8">
-                  <div className="w-full px-4 mb-6 md:w-4/6 lg:w-6/12 md:mb-0">
-                    <div className="flex flex-wrap items-center -mx-4">
-                      <div className="w-full px-4 mb-3 md:w-1/3">
-                        <div className="w-full h-96 md:h-24 md:w-24">
-                          <img
-                            src="https://i.postimg.cc/CLWkvq6f/pexels-markus-spiske-1002638.jpg"
-                            alt=""
-                            className="object-cover w-full h-full"
-                          />
-                        </div>
-                      </div>
-                      <div className="w-2/3 px-4">
-                        <h2 className="mb-2 text-xl font-bold dark:text-gray-400">
-                          DSL Camera
-                        </h2>
-                        <p className="text-gray-500 dark:text-gray-400 ">
-                          Picture frame
-                        </p>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="hidden px-4 lg:block lg:w-2/12">
-                    <p className="text-lg font-bold text-blue-500 dark:text-gray-400">
-                      $99.00
-                    </p>
-                    <span className="text-xs text-gray-500 line-through dark:text-gray-400">
-                      $1500
-                    </span>
-                  </div>
-                  <div className="w-auto px-4 md:w-1/6 lg:w-2/12 ">
-                    <QuantityButton/>
-                  </div>
-                  <div className="w-auto px-4 text-right md:w-1/6 lg:w-2/12 ">
-                    <p className="text-lg font-bold text-blue-500 dark:text-gray-400">
-                      $99.00
-                    </p>
-                  </div>
-                </div>
+                ))}
               </div>
               <div className="flex flex-wrap items-center gap-4">
                 <span className="">
