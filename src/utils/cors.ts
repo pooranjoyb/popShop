55983b8400cf8e65b56548029e4e052d4ebd03c7
Soni Dhruv <dhruvrsoni04@gmail.com>
2024-06-09 18:58:37 +0530

    remove unnessary file

--- src/utils/cors.ts
+++
@@ -1,6 +0,0 @@
-// cors.ts
-export const corsHeaders = {
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type',
-  };
-  
