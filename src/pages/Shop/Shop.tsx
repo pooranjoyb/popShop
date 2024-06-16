c45da7b1c886a421d0eb015db4d947d7bdd948f6
Soni Dhruv <dhruvrsoni04@gmail.com>
2024-06-16 20:32:46 +0530

    implementing search in products

--- src/pages/Shop/Shop.tsx
+++ src/pages/Shop/Shop.tsx
@@ -6,36 +6,31 @@ import Product from "../../components/Product";
 import { IoFilterCircleOutline } from "react-icons/io5";
 import { supabase } from "../../utils/client";
 
-
 function Shop() {
-
-  const [products, setproducts] = useState<any[]>([]);
+  const [products, setProducts] = useState<any[]>([]);
+  const [searchTerm, setSearchTerm] = useState<string>("");
 
   const getProducts = async () => {
-    const { data } = await supabase.from('Product_table').select()
-    console.log(data)
-    if(data){
-      setproducts(data)
+    const { data } = await supabase.from('Product_table').select();
+    console.log(data);
+    if (data) {
+      setProducts(data);
     }
-  }
+  };
 
   useEffect(() => {
-    getProducts()
+    getProducts();
     window.scrollTo(0, 0);
   }, []);
 
-  const handlesearch = async (e: any) => {
-    const { data } = await supabase.from('Product_table').select()
+  const handleSearch = (e: any) => {
     const value = e.target.value;
-    console.log(value, "hello");
-    if(data){
-      const filtereddata = data.filter((elem) => {
-        if (elem.name.toLowerCase().includes(value.toLowerCase())) return true;
-        else return;
-      });
-      setproducts(filtereddata);
-    }
+    setSearchTerm(value.toLowerCase());
   };
+
+  const filteredProducts = products.filter((product) =>
+    product.Name.toLowerCase().includes(searchTerm)
+  );
 
   const filter = [
     {
@@ -56,7 +51,7 @@ function Shop() {
 
   return (
     <>
-      <div className=" mx-auto max-w-screen-xl px-4 pt-12 pb-8 flex justify-center md:justify-between items-center flex-wrap">        
+      <div className="mx-auto max-w-screen-xl px-4 pt-12 pb-8 flex justify-center md:justify-between items-center flex-wrap">
         <Head h1="Our" h2="Store" />
         <div className="flex gap-6 mt-8 justify-center md:justify-end w-full">
           {/* The button to open modal */}
@@ -64,9 +59,9 @@ function Shop() {
             htmlFor="my_modal_6"
             className="btn bg-mygreen hover:bg-myyellow"
           >
-            <IoFilterCircleOutline className="text-3xl"/>
+            <IoFilterCircleOutline className="text-3xl" />
           </label>
-          {/* Modal Body*/}
+          {/* Modal Body */}
           <input
             type="checkbox"
             id="my_modal_6"
@@ -81,24 +76,22 @@ function Shop() {
                 {filter.map((fil, idx) => (
                   <div key={idx}>
                     <h2 className="mb-3">{fil.filterOption}</h2>
-                    {fil.checkbox.map((size, index) => {
-                      return (
-                        <div key={index} className="flex items-center mb-2">
-                          <input
-                            id={`default-checkbox${idx}${index}`}
-                            type="checkbox"
-                            value=""
-                            className="w-4 h-4 checkbox"
-                          />
-                          <label
-                            htmlFor={`default-checkbox${idx}${index}`}
-                            className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-                          >
-                            {size}
-                          </label>
-                        </div>
-                      );
-                    })}
+                    {fil.checkbox.map((size, index) => (
+                      <div key={index} className="flex items-center mb-2">
+                        <input
+                          id={`default-checkbox${idx}${index}`}
+                          type="checkbox"
+                          value=""
+                          className="w-4 h-4 checkbox"
+                        />
+                        <label
+                          htmlFor={`default-checkbox${idx}${index}`}
+                          className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                        >
+                          {size}
+                        </label>
+                      </div>
+                    ))}
                   </div>
                 ))}
               </div>
@@ -110,12 +103,12 @@ function Shop() {
                 >
                   Apply
                 </label>
-             
+
                 <label
-                 htmlFor="my_modal_6"
-                className="btn hover:bg-myred bg-myred"
+                  htmlFor="my_modal_6"
+                  className="btn hover:bg-myred bg-myred"
                 >
-                 Cancel
+                  Cancel
                 </label>
               </div>
             </div>
@@ -126,7 +119,7 @@ function Shop() {
               type="text"
               className="grow w-36"
               placeholder="Search"
-              onChange={handlesearch}
+              onChange={handleSearch}
             />
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -145,17 +138,15 @@ function Shop() {
       </div>
       <div className="mx-auto max-w-2xl px-4 py-8 lg:max-w-7xl lg:px-8">
         <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-          {products.map((elem, idx) => {
-            return (
-              <Product
-                key={idx}
-                desc={elem.Desc}
-                image={elem.Image_link}
-                price={elem.Price}
-                name={elem.Name}
-              />
-            );
-          })}
+          {filteredProducts.map((elem, idx) => (
+            <Product
+              key={idx}
+              desc={elem.Desc}
+              image={elem.Image_link}
+              price={elem.Price}
+              name={elem.Name}
+            />
+          ))}
         </div>
       </div>
     </>
