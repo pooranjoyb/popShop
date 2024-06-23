2b2b3bfc15a00fb1f257f85f9fd543b7ba74a306
Sandip Gyawali <sandipgyawali10@gmail.com>
2024-06-23 16:16:35 +0545

    shop-product conflict resolved

--- src/pages/Shop/Shop.tsx
+++ src/pages/Shop/Shop.tsx
@@ -1,4 +1,4 @@
-import React, { ChangeEvent, useEffect, useState } from "react";
+import { ChangeEvent, useEffect, useState } from "react";
 
 // components
 import Head from "../../components/Head";
@@ -6,25 +6,9 @@ import Product from "../../components/Product";
 import { IoFilterCircleOutline } from "react-icons/io5";
 import { supabase } from "../../utils/client";
 
-type PriceRange = {
-  min: number;
-  max: number;
-};
-
-type PriceRanges = {
-  [key: string]: PriceRange;
-};
-
 function Shop() {
-  const [products, setProducts] = useState<any[]>([]);
-  const [searchTerm, setSearchTerm] = useState<string>("");
-  const [selectedPriceRanges, setSelectedPriceRanges] = useState<string[]>([]);
-  const [tempSelectedPriceRanges, setTempSelectedPriceRanges] = useState<string[]>([]); // Temporary state for filter selections
-
-  useEffect(() => {
-    getProducts();
-    window.scrollTo(0, 0);
-  }, []);
+  const [products, setproducts] = useState<any[]>([]);
+  const [skeleton, setSkeleton] = useState<boolean>(true); //skeleton-state
 
   const getProducts = async () => {
     const { data } = await supabase.from("Product_table").select();
@@ -63,31 +47,13 @@ function Shop() {
       });
       setproducts(filtereddata);
     }
-
-    return selectedPriceRanges.some((range) => {
-      const priceRange = priceRanges[range];
-      if (priceRange) {
-        return (
-          product.Price >= priceRange.min && product.Price <= priceRange.max
-        );
-      }
-      return false;
-    });
-  };
-
-  const applyFilters = () => {
-    setSelectedPriceRanges(tempSelectedPriceRanges);
-  };
-
-  const priceRanges: PriceRanges = {
-    "below-₹1000": { min: 0, max: 999 },
-    "₹1000-₹2000": { min: 1000, max: 2000 },
-    "₹2000-₹4000": { min: 2000, max: 4000 },
-    "₹4000-₹8000": { min: 4000, max: 8000 },
-    "₹8000-and above": { min: 8000, max: Infinity },
   };
 
   const filter = [
+    {
+      filterOption: "Size",
+      checkbox: ["XS", "S", "M", "L", "XL"],
+    },
     {
       filterOption: "Price Range",
       checkbox: [
@@ -100,21 +66,19 @@ function Shop() {
     },
   ];
 
-  const filteredProducts = products
-    .filter((product) => product.Name.toLowerCase().includes(searchTerm))
-    .filter(filterByPrice);
-
   return (
     <>
-      <div className="mx-auto max-w-screen-xl px-4 pt-12 pb-8 flex justify-center md:justify-between items-center flex-wrap">
+      <div className=" mx-auto max-w-screen-xl px-4 pt-12 pb-8 flex justify-center md:justify-between items-center flex-wrap">
         <Head h1="Our" h2="Store" />
         <div className="flex gap-6 mt-8 justify-center md:justify-end w-full">
+          {/* The button to open modal */}
           <label
             htmlFor="my_modal_6"
             className="btn bg-mygreen hover:bg-myyellow"
           >
             <IoFilterCircleOutline className="text-3xl" />
           </label>
+          {/* Modal Body*/}
           <input
             type="checkbox"
             id="my_modal_6"
@@ -129,33 +93,32 @@ function Shop() {
                 {filter.map((fil, idx) => (
                   <div key={idx}>
                     <h2 className="mb-3">{fil.filterOption}</h2>
-                    {fil.checkbox.map((range, index) => (
-                      <div key={index} className="flex items-center mb-2">
-                        <input
-                          id={`default-checkbox${idx}${index}`}
-                          type="checkbox"
-                          value={range}
-                          className="w-4 h-4 checkbox"
-                          onChange={handlePriceRangeChange}
-                          checked={tempSelectedPriceRanges.includes(range)} // Maintain checked state in modal
-                        />
-                        <label
-                          htmlFor={`default-checkbox${idx}${index}`}
-                          className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-                        >
-                          {range}
-                        </label>
-                      </div>
-                    ))}
+                    {fil.checkbox.map((size, index) => {
+                      return (
+                        <div key={index} className="flex items-center mb-2">
+                          <input
+                            id={`default-checkbox${idx}${index}`}
+                            type="checkbox"
+                            value=""
+                            className="w-4 h-4 checkbox"
+                          />
+                          <label
+                            htmlFor={`default-checkbox${idx}${index}`}
+                            className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                          >
+                            {size}
+                          </label>
+                        </div>
+                      );
+                    })}
                   </div>
                 ))}
               </div>
 
-              <div className="modal-action pe-5">
+              <div className="modal-action  pe-5">
                 <label
                   htmlFor="my_modal_6"
                   className="btn hover:bg-mygreen bg-myyellow"
-                  onClick={applyFilters} // Apply button click handler
                 >
                   Apply
                 </label>
@@ -175,7 +138,7 @@ function Shop() {
               type="text"
               className="grow w-36"
               placeholder="Search"
-              onChange={handleSearch}
+              onChange={handlesearch}
             />
             <svg
               xmlns="http://www.w3.org/2000/svg"
