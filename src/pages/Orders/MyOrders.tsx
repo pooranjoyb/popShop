3c6a04931a8259a53ca6b39cdbdd387ac210aacd
Pooranjoy <pooranjoyb2016@gmail.com>
2024-06-29 13:23:13 +0530

    Fixed build errors

--- src/pages/Orders/MyOrders.tsx
+++ src/pages/Orders/MyOrders.tsx
@@ -1,4 +1,4 @@
-import Popup from './Popup'; 
+import Popup from "./Popup";
 import { Link } from "react-router-dom";
 import Head from "../../components/Head";
 import Button from "../../components/Button";
@@ -6,10 +6,22 @@ import { useEffect, useState } from "react";
 import { supabase } from "../../utils/client";
 import { useSelector } from "react-redux";
 import { RootState } from "../../utils/features/store";
+import Loader from "../../components/Loader/Loader";
+
+export interface Product {
+  desc: string | null;
+  image: string;
+  name: string;
+  price: number;
+  quantity: number;
+  ratings: number;
+  size: string;
+}
 
 export interface ORDER {
   username: string;
   orderId: string;
+  product: Product[];
   date: string;
   phone: number;
   status: string;
@@ -17,18 +29,18 @@ export interface ORDER {
 }
 
 const MyOrders = () => {
-  const [selectedOrder, setSelectedOrder] = useState(null);
+  const [selectedOrder, setSelectedOrder] = useState<ORDER | null>(null);
   const [orders, setOrders] = useState<ORDER[]>([]);
   const userName = useSelector((state: RootState) => state.auth.user.username);
 
-  const handleViewClick = (order) => {
+  const handleViewClick = (order: ORDER) => {
     setSelectedOrder(order);
   };
 
   const handleClosePopup = () => {
     setSelectedOrder(null);
   };
-    
+
   useEffect(() => {
     const fetchOrdersData = async () => {
       try {
@@ -39,7 +51,6 @@ const MyOrders = () => {
 
         if (error) throw error;
 
-        console.log(data);
         if (data) setOrders(data);
         else setOrders([]);
       } catch (err) {
@@ -50,59 +61,79 @@ const MyOrders = () => {
     if (userName) fetchOrdersData();
   }, [userName]);
 
+  const truncateText = (text: string, maxLength: number): string => {
+    return text.length > maxLength ? `${text.slice(0, maxLength)}...` : text;
+  };
+
   return (
     <>
-      <div className="mx-auto max-w-screen-xl px-4 pt-8 mt-8 sm:py-12">
-        <Head h1="My" h2="Orders" />
-      </div>
+      {orders ? (
+        <>
+          <div className="mx-auto max-w-screen-xl px-4 pt-8 mt-8 sm:py-12">
+            <Head h1="My" h2="Orders" />
+          </div>
 
-      <div className="w-2/3 mx-auto mb-32 mt-12">
-        <div className="overflow-x-auto rounded-lg border border-base-300">
-          <table className="table w-full">
-            {/* Table header */}
-            <thead>
-              <tr className="text-neutral">
-                <th>Order No.</th>
-                <th>Product Name</th>
-                <th>Price</th>
-                <th>Date</th>
-                <th>Status</th>
-                <th className="pl-12">Action</th>
-              </tr>
-            </thead>
-            <tbody>
-              {orders.map((order, index) => (
-                <tr key={index} className={index % 2 === 0 ? 'bg-base-200' : 'bg-base-100'}>
-                  <td>{order.orderNo}</td>
-                  <td>{order.productName}</td>
-                  <td>${order.price}</td>
-                  <td>{order.date}</td>
-                  <td>
-                    <span
-                      className={`badge ${
-                        order.status === 'Paid'
-                          ? 'badge-success badge-outline'
-                          : order.status === 'Cancelled'
-                          ? 'badge-error badge-outline'
-                          : 'badge-warning badge-outline'
-                      }`}
+          <div className="w-2/3 mx-auto mb-32 mt-12">
+            <div className="overflow-x-auto rounded-lg border border-base-300">
+              <table className="table w-full">
+                {/* Table header */}
+                <thead>
+                  <tr className="text-neutral">
+                    <th>Order No.</th>
+                    <th>Product Name</th>
+                    <th>Price</th>
+                    <th>Date</th>
+                    <th>Status</th>
+                    <th className="pl-12">Action</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {orders.map((order, index) => (
+                    <tr
+                      key={index}
+                      className={
+                        index % 2 === 0 ? "bg-base-200" : "bg-base-100"
+                      }
                     >
-                      {order.status}
-                    </span>
-                  </td>
-                  <td>
-                    <Link to="#">
-                      <Button text="View" color="mygreen" hover="myyellow" onClick={() => handleViewClick(order)} />
-                    </Link>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
-      </div>
-      {selectedOrder && (
-        <Popup order={selectedOrder} onClose={handleClosePopup} />
+                      <td>{truncateText(order.orderId, 10)}</td>
+                      <td>{order.product[0].name}</td>
+                      <td>${order.price}</td>
+                      <td>{order.date}</td>
+                      <td>
+                        <span
+                          className={`badge ${
+                            order.status === "Paid"
+                              ? "badge-success badge-outline"
+                              : order.status === "Cancelled"
+                              ? "badge-error badge-outline"
+                              : "badge-warning badge-outline"
+                          }`}
+                        >
+                          {order.status}
+                        </span>
+                      </td>
+                      <td>
+                        <Link to="#">
+                          <Button
+                            text="View"
+                            color="mygreen"
+                            hover="myyellow"
+                            onClick={() => handleViewClick(order)}
+                          />
+                        </Link>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          </div>
+          {selectedOrder && (
+            <Popup order={selectedOrder} onClose={handleClosePopup} />
+          )}
+        </>
+      ) : (
+        <Loader />
       )}
     </>
   );
--- src/pages/Orders/Popup.tsx
+++ src/pages/Orders/Popup.tsx
@@ -1,4 +1,29 @@
-const Popup = ({ order, onClose }) => {
+interface Product {
+  desc: string | null;
+  image: string;
+  name: string;
+  price: number;
+  quantity: number;
+  ratings: number;
+  size: string;
+}
+
+interface ORDER {
+  username: string;
+  orderId: string;
+  product: Product[];
+  date: string;
+  phone: number;
+  status: string;
+  price: string;
+}
+
+interface PopupProps {
+  order: ORDER;
+  onClose: () => void;
+}
+
+const Popup: React.FC<PopupProps> = ({ order, onClose }) => {
     return (
       <div className="fixed inset-0 flex items-center justify-center z-50">
         {/* Overlay */}
@@ -13,10 +38,10 @@ const Popup = ({ order, onClose }) => {
           </button>
           <h2 className="font-bold mb-4 text-2xl flex items-center justify-center text-myred">INVOICE</h2>
           <div className="mb-4 text-lg">
-            <strong>Order No:</strong> {order.orderNo}
+            <strong>Order No:</strong> {order.orderId}
           </div>
           <div className="mb-4 text-lg">
-            <strong>Product Name:</strong> {order.productName}
+            <strong>Product Name:</strong> {order.product[0].name}
           </div>
           <div className="mb-4 text-lg">
             <strong>Price:</strong> ${order.price}
