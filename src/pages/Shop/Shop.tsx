0dab96f83377e163c65744d766881691dbb0a723
Varsani2520 <varsaniranjani2520@gmail.com>
2024-07-27 12:50:45 +0530

    product card as well as img

--- src/pages/Shop/Shop.tsx
+++ src/pages/Shop/Shop.tsx
@@ -17,7 +17,8 @@ function Shop() {
     console.log(data);
     if (data) {
       setProducts(data);
-      setSearchResults(data); // Initially display all products
+      setSearchResults(data); 
+      console.log("daata",data)// Initially display all products
     }
   };
 
