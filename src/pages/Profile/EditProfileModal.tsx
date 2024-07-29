7634d04ffe403b63291c40eca88a4453c41987b9
Varsani2520 <varsaniranjani2520@gmail.com>
2024-07-29 09:41:44 +0530

    sync

--- src/pages/Profile/EditProfileModal.tsx
+++ src/pages/Profile/EditProfileModal.tsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import { supabase } from "../../utils/client";
 import { z } from "zod";
-import validator from "validator";
 
 interface USER {
     username: string;
