f0e79aac290cd4cc91419ccc0c27c7049607d21a
Varsani2520 <varsaniranjani2520@gmail.com>
2024-08-09 19:06:07 +0530

    rating display dynamically

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -8,54 +8,64 @@ interface Data {
   image: string;
   price: number;
   desc: string;
+  rating: string;
 }
 
-function Product({ name, image, price, desc }: Data) {
+function Product({ name, image, price, desc, rating }: Data) {
   const navigate = useNavigate();
   const [liked, setLiked] = useState<boolean>(false);
 
   const handleNavigate = () => {
-    navigate("/home/shop/product", { state: { name, image, price, desc } });
+    navigate("/home/shop/product", {
+      state: { name, image, price, desc, rating }
+    });
+    console.log("Product rating ", rating);
   };
+
   const handleLike = async () => {
     setLiked(!liked);
-        // If product is liked, add it to Supabase
     if (!liked) {
       const { error } = await supabase
         .from("liked_products")
         .insert([
-          { name, image, price, desc, created_at: new Date().toISOString() }
+          {
+            name,
+            image,
+            price,
+            desc,
+            created_at: new Date().toISOString(),
+            rating
+          }
         ]);
-        if(error){
-          console.error("error inserting product into supabase",error)
-        }
+      if (error) {
+        console.error("Error inserting product into Supabase", error);
+      }
     }
   };
-  return (
-    <>
-      <div className="flex flex-col">
-        <div
-          className={`relative top-[10%] left-[75%] p-2 z-10 rounded-full ${
-            liked ? "text-red-500" : "text-gray-500"
-          } hover:text-red-500 transition-colors duration-300 cursor-pointer`}
-          onClick={handleLike}
-        >
-          <HiHeart size={24} color={liked ? "red" : "white"} />
-        </div>
-        <button onClick={handleNavigate}>
-          <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
-            <img
-              src={image}
-              alt="Tall slender porcelain bottle with natural clay textured body and cork stopper."
-              className="h-[500px] w-full object-cover object-center  "
-            />
-          </div>
 
-          <h3 className="mt-4 text-sm text-gray-700">{name}</h3>
-          <p className="mt-1 text-lg font-medium text-gray-900">₹{price}</p>
-        </button>
+  return (
+    <div className="flex flex-col">
+      <div
+        className={`relative top-[10%] left-[75%] p-2 z-10 rounded-full ${
+          liked ? "text-red-500" : "text-gray-500"
+        } hover:text-red-500 transition-colors duration-300 cursor-pointer`}
+        onClick={handleLike}
+      >
+        <HiHeart size={24} color={liked ? "red" : "white"} />
       </div>
-    </>
+      <button onClick={handleNavigate}>
+        <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
+          <img
+            src={image}
+            alt={name}
+            className="h-[500px] w-full object-cover object-center"
+          />
+        </div>
+        <h3 className="mt-4 text-sm text-gray-700">{name}</h3>
+        <p className="mt-1 text-lg font-medium text-gray-900">₹{price}</p>
+       
+      </button>
+    </div>
   );
 }
 
--- src/pages/Shop/ProductDetail.tsx
+++ src/pages/Shop/ProductDetail.tsx
@@ -1,8 +1,8 @@
 import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
-import { toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import { useNavigate } from 'react-router-dom';
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { useNavigate } from "react-router-dom";
 
 // components
 import Head from "../../components/Head";
@@ -12,261 +12,263 @@ import { useDispatch, useSelector } from "react-redux";
 import { supabase } from "../../utils/client";
 
 export interface Data {
-    name: string;
-    image: string;
-    price: number;
-    desc: string;
-    qauntity: number;
+  name: string;
+  image: string;
+  price: number;
+  desc: string;
+  qauntity: number;
+  rating: string;
 }
 
 interface RatingItem {
-    className: string;
-    checked?: boolean;
+  className: string;
+  checked?: boolean;
 }
 
 interface CartItem {
-    name: string;
-    size: string;
-    quantity: number;
-  }
-  
+  name: string;
+  size: string;
+  quantity: number;
+}
 
 export interface UserState {
-    user: {
-        username: string;
-    };
+  user: {
+    username: string;
+  };
 }
 
 export interface RootState {
-    auth: UserState;
+  auth: UserState;
 }
 
 const commonClasses = "mask mask-star-2";
 
 const ratingItems: RatingItem[] = [
-    { className: `${commonClasses} mask-half-1` },
-    { className: `${commonClasses} mask-half-2` },
-    { className: `${commonClasses} mask-half-1` },
-    { className: `${commonClasses} mask-half-2` },
-    { className: `${commonClasses} mask-half-1` },
-    { className: `${commonClasses} mask-half-2` },
-    { className: `${commonClasses} mask-half-1` },
-    { className: `${commonClasses} mask-half-2` },
-    { className: `${commonClasses} mask-half-1` },
-    { className: `${commonClasses} mask-half-2` },
+  { className: `${commonClasses} mask-half-1` },
+  { className: `${commonClasses} mask-half-2` },
+  { className: `${commonClasses} mask-half-1` },
+  { className: `${commonClasses} mask-half-2` },
+  { className: `${commonClasses} mask-half-1` },
+  { className: `${commonClasses} mask-half-2` },
+  { className: `${commonClasses} mask-half-1` },
+  { className: `${commonClasses} mask-half-2` },
+  { className: `${commonClasses} mask-half-1` },
+  { className: `${commonClasses} mask-half-2` }
 ];
 
 function ProductDetail() {
-    const [filledStars, setFilledStars] = useState(0);
-    const availableSizes = ["XS", "S", "M", "L", "XL"];
-    const [size, setSize] = useState("");
-    const navigate = useNavigate();
+  const [filledStars, setFilledStars] = useState(0);
+  const availableSizes = ["XS", "S", "M", "L", "XL"];
+  const [size, setSize] = useState("");
+  const navigate = useNavigate();
 
-    const handleRatingChange = (index: number) => {
-        setFilledStars(index / 2 + 0.5);
-    };
+  const handleRatingChange = (index: number) => {
+    setFilledStars(index / 2 + 0.5);
+  };
 
-    const handleSize = (sizeValue: string) => {
-        setSize(sizeValue);
-    };
+  const handleSize = (sizeValue: string) => {
+    setSize(sizeValue);
+  };
 
-    const { state } = useLocation();
-    const data = state as Data;
+  const { state } = useLocation();
+  const data = state as Data;
 
-    useEffect(() => {
-        window.scrollTo(0, 0);
-    }, []);
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
 
-    const dispatch = useDispatch();
-    const userName = useSelector((state: RootState) => state.auth.user.username);
-    const product = {
+  const dispatch = useDispatch();
+  const userName = useSelector((state: RootState) => state.auth.user.username);
+  const product = {
+    name: data.name,
+    image: data.image,
+    price: data.price,
+    desc: data.desc,
+    quantity: data.qauntity || 1,
+    ratings: data.rating,
+    size //Include size
+  };
+  const addToCart = async () => {
+    if (!size) {
+      // added check for size
+      toast.error("Please select a size");
+      return;
+    }
+    try {
+      const product = {
         name: data.name,
         image: data.image,
         price: data.price,
         desc: data.desc,
         quantity: data.qauntity || 1,
-        ratings: 5,
-        size, //Include size
-    };
-    const addToCart = async () => {
-        if (!size) { // added check for size
-            toast.error('Please select a size');
-            return;
+        ratings: data.rating,
+        size // Include size
+      };
+
+      // Attempt to fetch the user's cart
+      const { data: userCart, error: fetchError } = await supabase
+        .from("Cart")
+        .select("*")
+        .eq("username", userName)
+        .single();
+
+      if (fetchError && fetchError.code !== "PGRST116") {
+        // Ignore "No such record" error
+        console.error("Fetch error:", fetchError);
+        throw fetchError;
+      }
+
+      console.log("Product to be added", product);
+
+      if (userCart) {
+        // Check if the product already exists in the cart
+        const existingProductIndex = userCart.products.findIndex(
+          (item: CartItem) =>
+            item.name === product.name && item.size === product.size
+        );
+
+        let updatedProducts;
+        if (existingProductIndex !== -1) {
+          // If the product exists, increase its quantity
+          updatedProducts = [...userCart.products];
+          updatedProducts[existingProductIndex].quantity += product.quantity;
+        } else {
+          // If the product does not exist, add it to the cart
+          updatedProducts = [...userCart.products, product];
         }
-        try {
-            const product = {
-                name: data.name,
-                image: data.image,
-                price: data.price,
-                desc: data.desc,
-                quantity: data.qauntity || 1,
-                ratings: 5,
-                size, // Include size
-            };
-    
-            // Attempt to fetch the user's cart
-            const { data: userCart, error: fetchError } = await supabase
-                .from('Cart')
-                .select('*')
-                .eq('username', userName)
-                .single();
-    
-            if (fetchError && fetchError.code !== 'PGRST116') { // Ignore "No such record" error
-                console.error("Fetch error:", fetchError);
-                throw fetchError;
-            }
-    
-            console.log("Product to be added", product);
-    
-            if (userCart) {
-                // Check if the product already exists in the cart
-                const existingProductIndex = userCart.products.findIndex(
-                (item: CartItem) => item.name === product.name && item.size === product.size
-  );
-    
-                let updatedProducts;
-                if (existingProductIndex !== -1) {
-                    // If the product exists, increase its quantity
-                    updatedProducts = [...userCart.products];
-                    updatedProducts[existingProductIndex].quantity += product.quantity;
-                } else {
-                    // If the product does not exist, add it to the cart
-                    updatedProducts = [...userCart.products, product];
-                }
-    
-                const { error: updateError } = await supabase
-                    .from('Cart')
-                    .update({ products: updatedProducts })
-                    .eq('username', userName);
-    
-                if (updateError) {
-                    console.error("Update error:", updateError);
-                    throw updateError;
-                }
-    
-                console.log("Product added/updated in cart:", product);
-                dispatch(addItem({ item: product }));
-                toast.success('Product added to cart');
-            } else {
-                // If the cart does not exist, insert a new row
-                const { error: insertError } = await supabase
-                    .from('Cart')
-                    .insert([
-                        {
-                            username: userName,
-                            products: [product],
-                        },
-                    ]);
-    
-                if (insertError) {
-                    console.error("Insert error:", insertError);
-                    throw insertError;
-                }
-    
-                console.log("Product added to cart:", product);
-                dispatch(addItem({ item: product }));
-                toast.success('Product added to cart');
-            }
-        } catch (error) {
-            console.error("Error adding product to cart:", error);
-            toast.error('Error adding product to cart');
+
+        const { error: updateError } = await supabase
+          .from("Cart")
+          .update({ products: updatedProducts })
+          .eq("username", userName);
+
+        if (updateError) {
+          console.error("Update error:", updateError);
+          throw updateError;
         }
-    };
-    
-    
 
-    return (
-        <>
-            <div className=" mx-auto max-w-screen-xl px-4 py-12 flex justify-between items-center">
-                <Head h1="Product" h2="Detail" />
-            </div>
+        console.log("Product added/updated in cart:", product);
+        dispatch(addItem({ item: product }));
+        toast.success("Product added to cart");
+      } else {
+        // If the cart does not exist, insert a new row
+        const { error: insertError } = await supabase.from("Cart").insert([
+          {
+            username: userName,
+            products: [product]
+          }
+        ]);
 
-            <section className="overflow-hidden bg-white py-11 font-poppins dark:bg-gray-800">
-                <div className="max-w-6xl px-4 py-4 mx-auto lg:py-8 md:px-6">
-                    <div className="flex flex-wrap -mx-4">
-                        <div className="w-full px-4 md:w-1/2 ">
-                            <div className="sticky top-0 z-50 overflow-hidden ">
-                                <div className="relative mb-6 lg:mb-10 lg:h-2/4 ">
-                                    <img src={data.image} alt=""
-                                        className="object-cover w-full lg:h-full " />
-                                </div>
-                            </div>
-                        </div>
-                        <div className="w-full px-4 md:w-1/2 ">
-                            <div className="lg:pl-20">
-                                <div className="mb-8 ">
-                                    <span className="text-lg font-medium text-rose-500 dark:text-rose-200">Buy Today</span>
-                                    <h2 className="max-w-xl mt-2 mb-6 text-2xl font-bold dark:text-gray-400 md:text-4xl">
-                                        {data.name}
-                                    </h2>
-                                    <p className="max-w-md mb-8 text-gray-700 dark:text-gray-400">
-                                        {data.desc}
-                                    </p>
-                                    <p className="inline-block mb-8 text-4xl font-bold text-gray-700 dark:text-gray-400 ">
-                                        <span>₹{data.price}</span>
-                                        <span
-                                            className="text-base font-normal text-gray-500 line-through dark:text-gray-400 ml-2">₹{data.price + 89}</span>
-                                    </p>
-                                    <p className="text-green-600 dark:text-green-300 ">7 in stock</p>
-                                </div>
-                                <div className="flex items-center mb-8">
-                                    <h2 className="w-16 text-xl font-bold dark:text-gray-400">
-                                        Size:</h2>
-                                    <div className="flex flex-wrap -mx-2 -mb-2">
-                                        {
-                                            availableSizes.map((value) => (
-                                                <button
-                                                    key={value}
-                                                    className={`py-1 mb-2 mr-1 border w-11 hover:border-blue-400 dark:border-gray-400 hover:text-blue-600 dark:hover:border-gray-300 dark:text-gray-400 ${value === size ? "bg-mygreen" : ""}`}
-                                                    onClick={() => { handleSize(value) }}>
-                                                    {value}
-                                                </button>
-                                            ))
-                                        }
-                                    </div>
-                                </div>
-                                <div className="rating rating-sm rating-half mb-8 items-center">
-                                    <h2 className="w-16 text-xl font-bold dark:text-gray-400">
-                                        Rating:
-                                    </h2>
-                                    <input
-                                        type="radio"
-                                        name="rating-10"
-                                        className="rating-hidden"
-                                        readOnly
-                                        onChange={() => handleRatingChange(-1)}
-                                        defaultChecked
-                                    />
-                                    {ratingItems.map((item, index) => (
-                                        <input
-                                            key={index}
-                                            type="radio"
-                                            name="rating-10"
-                                            className={item.className}
-                                            onChange={() => handleRatingChange(index)}
-                                            readOnly
-                                        />
-                                    ))}
-                                    <span className="ml-2">{`${filledStars} out of 5 `}
-                                    </span>
-                                </div>
-                                <div className="flex flex-wrap items-center gap-10 ">
-                                    <Button text="Add to Cart" color="mygreen" hover="myred" onClick={addToCart} />
-                                    <Button
-                                        text="Buy Now"
-                                        color="myyellow"
-                                        hover="myred"
-                                        onClick={() => navigate("/home/shop/checkout", { state: { directPurchase: product } })}
-                                    />
+        if (insertError) {
+          console.error("Insert error:", insertError);
+          throw insertError;
+        }
 
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+        console.log("Product added to cart:", product);
+        dispatch(addItem({ item: product }));
+        toast.success("Product added to cart");
+      }
+    } catch (error) {
+      console.error("Error adding product to cart:", error);
+      toast.error("Error adding product to cart");
+    }
+  };
+
+  return (
+    <>
+      <div className=" mx-auto max-w-screen-xl px-4 py-12 flex justify-between items-center">
+        <Head h1="Product" h2="Detail" />
+      </div>
+
+      <section className="overflow-hidden bg-white py-11 font-poppins dark:bg-gray-800">
+        <div className="max-w-6xl px-4 py-4 mx-auto lg:py-8 md:px-6">
+          <div className="flex flex-wrap -mx-4">
+            <div className="w-full px-4 md:w-1/2 ">
+              <div className="sticky top-0 z-50 overflow-hidden ">
+                <div className="relative mb-6 lg:mb-10 lg:h-2/4 ">
+                  <img
+                    src={data.image}
+                    alt=""
+                    className="object-cover w-full lg:h-full "
+                  />
                 </div>
-            </section>
-        </>
-    )
+              </div>
+            </div>
+            <div className="w-full px-4 md:w-1/2 ">
+              <div className="lg:pl-20">
+                <div className="mb-8 ">
+                  <span className="text-lg font-medium text-rose-500 dark:text-rose-200">
+                    Buy Today
+                  </span>
+                  <h2 className="max-w-xl mt-2 mb-6 text-2xl font-bold dark:text-gray-400 md:text-4xl">
+                    {data.name}
+                  </h2>
+                  <p className="max-w-md mb-8 text-gray-700 dark:text-gray-400">
+                    {data.desc}
+                  </p>
+                  <p className="inline-block mb-8 text-4xl font-bold text-gray-700 dark:text-gray-400 ">
+                    <span>₹{data.price}</span>
+                    <span className="text-base font-normal text-gray-500 line-through dark:text-gray-400 ml-2">
+                      ₹{data.price + 89}
+                    </span>
+                  </p>
+                  <p className="text-green-600 dark:text-green-300 ">
+                    7 in stock
+                  </p>
+                </div>
+                <div className="flex items-center mb-8">
+                  <h2 className="w-16 text-xl font-bold dark:text-gray-400">
+                    Size:
+                  </h2>
+                  <div className="flex flex-wrap -mx-2 -mb-2">
+                    {availableSizes.map((value) => (
+                      <button
+                        key={value}
+                        className={`py-1 mb-2 mr-1 border w-11 hover:border-blue-400 dark:border-gray-400 hover:text-blue-600 dark:hover:border-gray-300 dark:text-gray-400 ${
+                          value === size ? "bg-mygreen" : ""
+                        }`}
+                        onClick={() => {
+                          handleSize(value);
+                        }}
+                      >
+                        {value}
+                      </button>
+                    ))}
+                  </div>
+                </div>
+                <div className="mb-8">
+                  <h2 className="text-xl font-bold text-gray-800 dark:text-gray-200">
+                    Rating:{" "}
+                    <span className="bg-mygreen p-1 px-3 text-mywhite rounded-md">
+                      {product.ratings} ⭐
+                    </span>
+                  </h2>
+                </div>
+                <div className="flex flex-wrap items-center gap-10 ">
+                  <Button
+                    text="Add to Cart"
+                    color="mygreen"
+                    hover="myred"
+                    onClick={addToCart}
+                  />
+                  <Button
+                    text="Buy Now"
+                    color="myyellow"
+                    hover="myred"
+                    onClick={() =>
+                      navigate("/home/shop/checkout", {
+                        state: { directPurchase: product }
+                      })
+                    }
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
 
 export default ProductDetail;
